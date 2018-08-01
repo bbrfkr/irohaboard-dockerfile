34f55cef5408a35e067b8ba9d3b9fba508b59f74
bbrfkr <1.boogie.5.sophia.4.pop.0@gmail.com>
2018-08-01 11:14:03 +0900

    second

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,12 @@
 
 source /tmp/check_variables
 
+MARIADB_HOSTNAME=${MARIADB_HOSTNAME:-$MARIADB_PORT_3306_TCP_ADDR}
+
 echo "##########"
 echo "wait mysql is started"
 
-until mysql -uroot -p$MARIADB_ROOT_PASS -h$MARIADB_PORT_3306_TCP_ADDR -e "show databases;" > /dev/null 2>&1
+until mysql -uroot -p$MARIADB_ROOT_PASS -h$MARIADB_HOSTNAME -e "show databases;" > /dev/null 2>&1
 do
   sleep 1
 done
@@ -13,14 +15,14 @@ done
 echo "mysql has been started"
 echo "##########"
 
-mysql -uroot -p$MARIADB_ROOT_PASS -h$MARIADB_PORT_3306_TCP_ADDR -e "show databases;" | \
+mysql -uroot -p$MARIADB_ROOT_PASS -h$MARIADB_HOSTNAME -e "show databases;" | \
   grep "irohaboard" > /dev/null
 if [ $? -ne 0 ] ; then
   echo "##########"
   echo "create database 'irohaboard'" 
   echo "##########"
 
-  mysql -uroot -p$MARIADB_ROOT_PASS -h$MARIADB_PORT_3306_TCP_ADDR mysql <<EOF
+  mysql -uroot -p$MARIADB_ROOT_PASS -h$MARIADB_HOSTNAME mysql <<EOF
 CREATE DATABASE irohaboard;
 GRANT ALL PRIVILEGES ON irohaboard.* TO 'irohaboard'@'%' IDENTIFIED BY '$IROHABOARD_DB_PASS';
 FLUSH PRIVILEGES;
@@ -35,11 +37,12 @@ else
   echo "##########"
 fi
 
+
 echo "##########"
 echo "modify database.php"
 echo "##########"
 sed -i "s/IROHABOARD_DB_PASS/$IROHABOARD_DB_PASS/g" /var/www/html/Config/database.php
-sed -i "s/MARIADB_HOSTNAME/$MARIADB_PORT_3306_TCP_ADDR/g" /var/www/html/Config/database.php
+sed -i "s/MARIADB_HOSTNAME/$MARIADB_HOSTNAME/g" /var/www/html/Config/database.php
 
 echo "##########"
 echo "start apache"
